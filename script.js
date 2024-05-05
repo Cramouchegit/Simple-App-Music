7f2c99d0c8e3ce834e3842ac955c6e385564cc5d
Norman <norman@advancedgroup.com>
2024-05-05 23:47:35 +0700

    new update commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const audio = document.getElementById("audio");
 const playButton = document.getElementById("play_pause");
 
+loadDataMusik();
+
 playButton.addEventListener("click", () => {
   const isPause = audio.paused;
 
@@ -51,10 +53,73 @@ audio.addEventListener("pause", () => {
   document.querySelector(".player .album").classList.add("pause");
 });
 
+document.getElementById("next").addEventListener("click", async (e) => {
+  const currentPlay = audio.getAttribute("data-id");
+  const dataMusik = await getDataMusik();
+  const nextIndex = dataMusik.data.findIndex((m) => m.id == currentPlay) + 1;
+  const nextMusik = dataMusik.data[nextIndex];
+  updateMusicInfo(nextMusik);
+});
+
+document.getElementById("previous").addEventListener("click", async (e) => {
+  const currentPlay = audio.getAttribute("data-id");
+  const dataMusik = await getDataMusik();
+  const nextIndex = dataMusik.data.findIndex((m) => m.id == currentPlay) - 1;
+  const nextMusik = dataMusik.data[nextIndex];
+  updateMusicInfo(nextMusik);
+});
+
+async function loadDataMusik() {
+  const dataMusik = await getDataMusik();
+  const listMusik = dataMusik.data
+    .map((musik) => {
+      return `<li data-id="${musik.id}">${musik.title}</li>`;
+    })
+    .join("");
+
+  const ulElement = document.querySelector(".playlist ul");
+  ulElement.innerHTML = listMusik;
+  const li = document.querySelectorAll(".playlist ul li");
+
+  li.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      const dataMusikIni = dataMusik.data.filter((musik) => {
+        return musik.id == e.target.getAttribute("data-id");
+      })[0];
+
+      updateMusicInfo(dataMusikIni);
+    });
+  });
+}
+async function getDataMusik() {
+  const response = await fetch("data.json");
+  const dataMusik = await response.json();
+
+  return dataMusik;
+}
+function updateMusicInfo(dataMusik) {
+  document.querySelector(".player .info h2").innerText = dataMusik.title;
+  document.querySelector(".player .info p").innerText = dataMusik.artist;
+  document
+    .querySelector(".player .album img")
+    .setAttribute("src", dataMusik.album_cover);
+
+  audio.setAttribute("src", dataMusik.url);
+  audio.play();
+  audio.setAttribute("data-id", dataMusik.id);
+
+  document.querySelectorAll(".playlist ul li").forEach((li) => {
+    li.classList.remove("active");
+  });
+
+  document
+    .querySelector(`.playlist ul li[data-id='${dataMusik.id}']`)
+    .classList.add("active");
+}
+
 //Navbar
 const mobileNav = document.querySelector(".hamburger");
 const navbar = document.querySelector(".menubar");
-
 const toggleNav = () => {
   navbar.classList.toggle("active");
   mobileNav.classList.toggle("hamburger-active");
